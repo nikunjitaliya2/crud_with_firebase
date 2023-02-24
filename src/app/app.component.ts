799e524418cd9dd608d6b99e3bb43bbe965b1e7a
Nikunj_italiya <nikunj.italiya@ghanshyamdigital.com>
2023-02-24 18:56:01 +0530

    completed crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  collectionData,
+  doc,
+  updateDoc,
+  deleteDoc
+} from "@angular/fire/firestore";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-fire';
+  userData!:Observable<any>;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+
+  // insert user data
+  addData(f: any) {
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, f.value)
+      .then(() => {
+      console.log("saved successfully");
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  // read user data
+  getData(){
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance,{idField : 'id'})
+      .subscribe(res => {
+      // console.log(res);
+    })
+    this.userData = collectionData(collectionInstance , {idField : 'id'})
+  }
+
+  updateData(id: string){
+    const docInstance = doc(this.firestore,'users',id);
+    const updateData = {
+      name : 'element'
+    }
+    updateDoc(docInstance,updateData)
+      .then(()=> {
+        console.log('data updated');
+      })
+      .catch((err)=>{
+        console.log("err",err);
+      })
+  }
+
+  deleteUser(id: string){
+    const docInstance = doc(this.firestore,'users',id);
+    deleteDoc(docInstance)
+      .then(()=>{
+        console.log('data deleted');
+      })
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'checking-6a0ac',
+    appId: '1:570361663455:web:3bda38f105caa7cd663e6a',
+    storageBucket: 'checking-6a0ac.appspot.com',
+    apiKey: 'AIzaSyApyXpdV_owXpUogRGn2J8Mw1y_f6o30ow',
+    authDomain: 'checking-6a0ac.firebaseapp.com',
+    messagingSenderId: '570361663455',
+  },
+  production: true,
+};
